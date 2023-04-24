85f715f343abb8703aa230289efc8439ca9a045c
Nati Worku <natiworku1971@gmail.com>
2023-04-24 15:41:36 -0400

    add async

--- routes/book.js
+++ routes/book.js
@@ -4,14 +4,19 @@ var bookModel = require('../models').book
 
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-    res.json(bookModel.findAll())
+router.get("/", async (req, res) => {
+    const books = await bookModel.findAll();
+    res.json(books);
 });
-router.post('/',(req, res) => {
-    bookModel.create({
+router.post("/", (req, res) => {
+    readerModel.create({
         title: req.body.title,
-        author: req.body.author 
-    })
-    res.send('new book accepted')
+        author: req.body.author,
+    });
+    res.send("new book accepted");
+});
+router.get('/:id', async (req, res) => {
+    const book = await bookModel.findByPk(req.params.id);
+    res.json(book)
 })
 module.exports = router;
--- routes/reader.js
+++ routes/reader.js
@@ -1,18 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var readerModel = require('../models').reader
-
+var readerModel = require("../models").reader;
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-    const readers = readerModel.findAll()
-    res.send(readers)
+router.get("/", async (req, res) => {
+    const readers = await readerModel.findAll();
+    res.json(readers);
 });
-router.post('/',(req, res) => {
+router.post("/", (req, res) => {
     readerModel.create({
         name: req.body.name,
-        address: req.body.address 
-    })
-    res.send('new reader accepted')
+        address: req.body.address,
+    });
+    res.send("new reader accepted");
+});
+router.get('/:id', async (req, res) => {
+    const reader = await readerModel.findByPk(req.params.id);
+    res.json(reader)
 })
 module.exports = router;
